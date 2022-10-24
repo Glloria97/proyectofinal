02484fb4c2b2525ca6fb7f7fc60f9166f4acdee7
Glloria97 <113401780+Glloria97@users.noreply.github.com>
2022-10-23 23:04:20 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,85 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.QueOfrecemos', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-30px',
+     });
+   
+     sr.reveal('.actividadesContenedor', {
+      duration: 2000,
+      origin: 'bottom',
+      distance: '-30px',
+   });
+
+    sr.reveal('.acts1', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-30px',
+     });
+
+    sr.reveal('.acts2', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-30px',
+     });
+
+    sr.reveal('.valoresContenedor', {
+        duration: 3500,
+        origin: 'bottom',
+        distance: '-50px',
+     });
+
+    sr.reveal('.horarios', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-30px',
+     });
+    
+     sr.reveal('.NuevasSedes', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-30px',
+     });
+
+     sr.reveal('.DetalleSedes', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-50px',
+     });
+
+     sr.reveal('.formulario', {
+      duration: 2000,
+      origin: 'bottom',
+      distance: '-50px',
+   });
+
+   sr.reveal('.seccionnutricion', {
+      duration: 2000,
+      origin: 'bottom',
+      distance: '-50px',
+   });
+
+   sr.reveal('.seccionOutdoor', {
+      duration: 2500,
+      origin: 'bottom',
+      distance: '-50px',
+   });
+
+   sr.reveal('.secciontodos', {
+      duration: 3000,
+      origin: 'bottom',
+      distance: '-50px',
+   });
+
+   sr.reveal('.maraton', {
+      duration: 3000,
+      origin: 'bottom',
+      distance: '-50px',
+   });
+
+   sr.reveal('.FondoDiaDelNiño', {
+      duration: 3000,
+      origin: 'bottom',
+      distance: '-50px',
+   });
